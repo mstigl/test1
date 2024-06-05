bf7c8959a198aa3470800403ba546c32e48d852e
mstigl <159584512+mstigl@users.noreply.github.com>
2024-06-05 14:41:11 +0200

    Update test1.bat

--- test1.bat
+++ test1.bat
@@ -1,6 +1,6 @@
 @echo off
 :loop
-start "" "https://www.google.com"
+start "" /b "https://www.amazon.com"
 START %SystemRoot%\system32\notepad.exe
 START %SystemRoot%\system32\notepad.exe
 ping -n 1 127.0.0.1 > nul
